0b64c33e5bef2669411ce89cf1f713e58a8beee5
Hol <79064972+TrainerHol@users.noreply.github.com>
2021-09-10 12:00:28 -0700

    Claiming logic

--- contracts/MembershipCard.sol
+++ contracts/MembershipCard.sol
@@ -3,16 +3,21 @@ pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MembershipCard is ERC721, ERC721Enumerable, Ownable {
+contract MembershipCard is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
-    uint256 public intervals;
+    uint256 public constant interval = 1 * 10**17;
     // Distribution
-    mapping(uint256 => uint256) public cardStakes;
+    mapping(uint256 => uint256) public cardBalances;
+    mapping(uint256 => uint256) rewardSnapshots;
+    uint256 currentBalances;
+    uint256 bananaRewards;
+    uint256 bananaChange;
     CardTier[] public tiers;
 
     constructor(
@@ -23,14 +28,45 @@ contract MembershipCard is ERC721, ERC721Enumerable, Ownable {
         uint256[] memory _thresholds
     ) ERC721(_tokenName, _tokenSymbol) {
         transferOwnership(multisig);
+        setTiers(_tierNames, _thresholds);
+    }
+
+    function setTiers(string[] memory _tierNames, uint256[] memory _thresholds)
+        internal
+    {
         for (uint256 index = 0; index < _tierNames.length; index++) {
             tiers[index] = CardTier(_tierNames[index], _thresholds[index]);
         }
     }
 
+    function getCardPoints(uint256 _id) public view returns (uint256 val) {
+        val = cardBalances[_id] / interval;
+    }
+
     function safeMint(address to) public payable {
+        require(msg.value >= interval, "Low val");
         _safeMint(to, _tokenIdCounter.current());
+        uint256 id = _tokenIdCounter.current();
+        cardBalances[id] = msg.value;
+        currentBalances += msg.value / interval;
         _tokenIdCounter.increment();
+    }
+
+    function increaseCardBalance(uint256 _id) public payable {
+        require(msg.value >= interval, "Low val");
+        cardBalances[_id] += msg.value;
+        currentBalances += msg.value / interval;
+    }
+
+    function distributeBanana(uint256 _amount) public {
+        require(_amount > 0 && currentBalances > 0);
+        _amount += bananaChange;
+        bananaRewards += _amount / currentBalances;
+        bananaChange = _amount % currentBalances;
+    }
+
+    function claimBanana(uint256 _cardId) public {
+        //TODO: Claim banana function
     }
 
     function _beforeTokenTransfer(
@@ -48,6 +84,23 @@ contract MembershipCard is ERC721, ERC721Enumerable, Ownable {
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        // TODO: Dynamic metadata
+        return super.tokenURI(tokenId);
+    }
+
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
     }
 }
 
